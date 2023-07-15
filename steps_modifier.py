893152789bd0b81f2d7d2dc9f2f32c67fc77ca24
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-07-15 15:44:05 +0800

    Update and rename step.py to steps_modifier.py

---
+++ steps_modifier.py
@@ -0,0 +1,20 @@
+import requests
+
+def modify_steps(username, password, steps):
+    url = 'http://bs.svv.ink'  # 接口 URL
+    data = {
+        'username': username,
+        'password': password,
+        'steps': steps
+    }
+    response = requests.post(url, data=data)
+    return response.text
+
+if __name__ == '__main__':
+    # 在此处填入你的用户名、密码和要修改的步数
+    username = '15033296069'
+    password = '20030101h'
+    steps = 50000
+
+    result = modify_steps(username, password, steps)
+    print(result)
